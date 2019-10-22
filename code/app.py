2be26c6800d83135b56147fddd882cea1350cf8e
Francesco Uccelli <39736679+Flrotm@users.noreply.github.com>
2019-10-22 14:24:45 -0500

    Update app.py

--- code/app.py
+++ code/app.py
@@ -7,7 +7,12 @@ def index():
     return render_template('home.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 if __name__=="__main__":
     app.run(debug=True)
+
+
