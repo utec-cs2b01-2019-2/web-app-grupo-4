f40c28562a231f555957e32514ad01048b20f283
Francesco Uccelli <39736679+Flrotm@users.noreply.github.com>
2019-10-24 17:04:44 -0500

    Update wtforms_fields.py

--- code/wtforms_fields.py
+++ code/wtforms_fields.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,IntegerField
+from wtforms import StringField,PasswordField,IntegerField,DateField
 from wtforms.validators import InputRequired, Length, EqualTo, ValidationError
 from models import *
 
@@ -10,4 +10,13 @@ class RegisterForm(FlaskForm):
     email = StringField('Email', validators=[InputRequired(message="Username required"), Length(min=4, max=100,message="Ingrese su correo electronico")])
     password = PasswordField('Contrasena', validators=[InputRequired(message="Password required"), Length(min=4, max=25, message="La contrasena debe tener entre 4 y 25 caracteres")])
     confirm = PasswordField('Confirmar contrasena', validators=[InputRequired(message="Password required"), EqualTo('password', message="Las contrasenas deben ser iguales")])
+
+class ValidarPedido(FlaskForm):
+    """ Pedido form"""
+    nombre_cliente = StringField('Nombre del local', validators=[InputRequired(message="Nombre requerido"), Length(min=2, max=100, message="")])
+    cantidad = IntegerField('Cantidad de bolsas', validators=[InputRequired(message="Ingrese la cantidad de bolsas")])
+    tipo = StringField('Tipo de bolsa 3kg o 5kg', validators=[InputRequired(message="Ingrese el tipo de bolsa")])
+    fecha_entrega = DateField('Fecha de entrega', validators=[InputRequired(message="Escoga una fecha de entrega")],format='%d/%m/%Y')
+
+
 
