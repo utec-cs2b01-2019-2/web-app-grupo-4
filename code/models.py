8b087519c16b532dacab73588b222f0668c6a41c
Francesco Uccelli <39736679+Flrotm@users.noreply.github.com>
2019-10-24 16:13:59 -0500

    Update models.py

--- code/models.py
+++ code/models.py
@@ -1,17 +1,30 @@
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 from flask_login import UserMixin
+from wtforms import *
+import wtforms_fields
 
 db = SQLAlchemy()
 
 
-class User(UserMixin, db.Model):
-    """ User model """
 
+class User(db.Model):
+    """ User model """
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     name=db.Column(db.String(25),unique=True, nullable=False)
     email = db.Column(db.String(25), unique=True, nullable=False)
     username = db.Column(db.String(25), unique=True, nullable=False)
-    hashed_pswd = db.Column(db.String(), nullable=False)
-    created = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    password = db.Column(db.String(), nullable=False)
+    register_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+class Pedido(db.Model):
+    __tablename__ = "pedidos"
+    id = db.Column(db.Integer, primary_key=True)
+    nombre_cliente = db.Column(db.String(36), unique=False, nullable=True)
+    fecha_entrega = db.Column(db.DateTime(25), unique=False, nullable=False)
+    cantidad = db.Column(db.Integer, unique=False, nullable=False)
+    cancelada = db.Column(db.String(3), nullable=False)
+    fecha_pedido = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+
